be0e26e11dcc7fcbe856613c0237c71cd566ae79
jsamuel <youreyesight@gmail.com>
2017-12-23 15:29:55 +0800

    init

---
+++ src/components/common/header.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="header flex">
+    <span class="header-back" @click="returnHandle">
+      <span  v-if="back">
+        <em v-if="isTextRight">{{backText}}</em>
+        <i icon="close" v-else></i>
+      </span>
+    </span>
+    <span class="header-title" flex="1">{{title}}</span>
+    <span class="header-handle" @click="buttonHandle">{{handleText}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'c-head',
+  props:{
+    "back":{
+      type:Boolean,
+      default:true
+    },
+    "title":{
+      type:String,
+      default:'头部'
+    },
+    "backText":{
+      type:String,
+      default:'返回'
+    },
+    "handleText":{
+      type:String,
+      default:''
+    },
+    "returnHandle":{
+      type:Function,
+      default(){
+        console.log('dasda')
+      }
+    },
+    "buttonHandle":{
+      type:Function,
+      default(){
+        console.log('dasda')
+      }
+    }
+  },
+  data(){
+    return {
+        isTextRight:!!this.leftText
+    }
+  },
+  methods:{
+  }
+}
+</script>
+<style scoped>
+.header{
+  position: fixed;
+  left: 0;
+  top: 0;
+  height: 44px;
+  line-height: 44px;
+  box-shadow: 0 1px 1px 1px #ccc;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: space-between;
+  box-sizing: border-box;
+}
+.header-back,.header-handle{
+  width: 60px;
+}
+</style>
+
---
+++ src/components/common/ui/uiTab.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="ui-tab">
+    <ul class="ui-tab-wrapper flex">
+      <li class="ui-tab-item" :class="{'current-item':item.id == currentIndex ? true : false}" flex="1 row v-center h-center" :key="item.id" :id="item.id" @click="changeStatus" v-for="item in datamodel">
+        {{item.name}}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name:'ui-tab',
+  props:{
+    "datamodel":{
+      type:Array,
+      default:[{
+        name:'按钮1',
+        id:0
+      },{
+        name:'按钮2',
+        id:1
+      }]
+    }
+  },
+  data(){
+     return{
+        currentIndex:0
+     }
+  },
+  methods:{
+      changeStatus(e){
+          this.$emit('changeTab',e);
+      }
+  }
+}
+</script>
+<style scoped>
+.ui-tab{
+  width:100%;
+  height:100%;
+}
+.ui-tab-wrapper{
+  height:100%;
+  width: 100%;
+}
+.ui-tab-item{
+  height:100%;
+  text-align: center;
+}
+.current-item{
+  background: #e8e8e8;
+}
+</style>
+
+
